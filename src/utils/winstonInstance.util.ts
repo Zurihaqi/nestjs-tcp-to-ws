0efc66ecc1d11ce0c034fcf9270bf1f2d5f922f4
Zurihaqi <zurihaqi@gmail.com>
2025-08-12 10:49:43 +0700

    fix: winston logger not creating file logs

--- src/utils/winstonInstance.util.ts
+++ src/utils/winstonInstance.util.ts
@@ -4,12 +4,18 @@ import {
 } from 'nest-winston';
 import * as winston from 'winston';
 import * as path from 'path';
+import * as fs from 'fs';
 import moment from 'moment';
 
 const logDir = path.join(process.cwd(), 'logs');
-const logName = `tcp-to-ws_${moment().format('YYYY-MM-DD_HH:mm:ss')}.log`;
+const logName = `tcp-to-ws_${moment().format('YYYY-MM-DD_HH-mm-ss')}.log`;
 
-const winstonFormat = winston.format.combine(
+// Memastikan directory log ada
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
+
+const consoleFormat = winston.format.combine(
   winston.format.timestamp({
     format: 'YYYY-MM-DD HH:mm:ss'
   }),
@@ -19,18 +25,26 @@ const winstonFormat = winston.format.combine(
   })
 );
 
+const fileFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  nestWinstonModuleUtilities.format.nestLike('TCP to WS', {
+    colors: false, // Txt tidak dapat membaca warna
+    prettyPrint: true
+  })
+);
+
 export const winstonInstance = WinstonModule.createLogger({
   transports: [
     // Log ke console
     new winston.transports.Console({
       level: 'debug',
-      format: winstonFormat
+      format: consoleFormat
     }),
     // Log ke file
     new winston.transports.File({
       filename: path.join(logDir, logName),
       level: 'debug',
-      format: winstonFormat
+      format: fileFormat
     })
   ]
 });
