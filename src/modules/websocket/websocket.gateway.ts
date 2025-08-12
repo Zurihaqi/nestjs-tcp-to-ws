3a62dca5dbb38d620fcac4e9cd49dbf3e72c5379
Zurihaqi <zurihaqi@gmail.com>
2025-08-12 11:22:07 +0700

    chore: add explicit OnModuleInit interface because why not

--- src/modules/websocket/websocket.gateway.ts
+++ src/modules/websocket/websocket.gateway.ts
@@ -1,12 +1,12 @@
 import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 
 const PORT = process.env.WS_PORT ?? 9472;
 
 @Injectable()
 @WebSocketGateway(+PORT, { cors: true })
-export class WebsocketGateway {
+export class WebsocketGateway implements OnModuleInit {
   private readonly logger = new Logger(WebsocketGateway.name);
 
   @WebSocketServer()
